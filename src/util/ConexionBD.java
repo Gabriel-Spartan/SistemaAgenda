b3de3d8c903acd557863776e9d9713e3b686eccc
Gabriel Llerena <gabriel0llerena@gmail.com>
2025-05-10 23:27:54 -0500

    Modificado para que funcione con la creación de un archivo .env

--- src/util/ConexionBD.java
+++ src/util/ConexionBD.java
@@ -1,15 +1,27 @@
 // autor: gabriel0llerena@gmail.com/Gabriel-Spartan
 package util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/agenda_db?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final Dotenv dotenv = Dotenv.configure()
+        .directory("./")        // Ruta al .env (raíz del proyecto)
+        .ignoreIfMissing()      // No falla si no existe (opcional)
+        .load();
+
+    private static final String HOST = dotenv.get("DB_HOST", "localhost");
+    private static final String PORT = dotenv.get("DB_PORT", "3306");
+    private static final String DB = dotenv.get("DB_NAME", "agenda_db");
+    private static final String USER = dotenv.get("DB_USER", "root");
+    private static final String PASSWORD = dotenv.get("DB_PASS", "");
+
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB +
+            "?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
